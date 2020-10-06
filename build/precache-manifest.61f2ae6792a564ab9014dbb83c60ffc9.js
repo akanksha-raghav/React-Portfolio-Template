9eef2cf50263dc30f57ba41c6977e4a2a6918264
Akanksha Raghav <bitturaghav919@gmail.com>
2020-10-06 20:54:30 +0530

    Add files via upload

---
+++ build/precache-manifest.61f2ae6792a564ab9014dbb83c60ffc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846c516bcc172059faf9287517157e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1932b09696803a8531e5",
+    "url": "/static/css/2.1a660e5d.chunk.css"
+  },
+  {
+    "revision": "de6485b79d2efce980de",
+    "url": "/static/css/main.a8cf077a.chunk.css"
+  },
+  {
+    "revision": "1932b09696803a8531e5",
+    "url": "/static/js/2.4ef20b9f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.4ef20b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6485b79d2efce980de",
+    "url": "/static/js/main.4a7934b9.chunk.js"
+  },
+  {
+    "revision": "8786cd60317b02073ca4",
+    "url": "/static/js/runtime-main.ee58f54d.js"
+  }
+]);
