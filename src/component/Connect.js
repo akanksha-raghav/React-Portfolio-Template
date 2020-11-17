55df547ea17cdf3ddd269ed4b2333699d3d1192a
Akanksha Raghav <akanksharaghav919@gmail.com>
2020-11-17 16:32:45 +0530

    Contact form css styling

--- src/component/Connect.js
+++ src/component/Connect.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Footer from './Footer';
+import Col from 'react-bootstrap/Col'; 
+import Container from 'react-bootstrap/Container';
+import { Row } from 'react-bootstrap';
 function Connect() {
     return (
         <div className="page">
         <section id="main-contact" className="section-5">
+        <Container>
+                    <Row xs={6} md={10}>
+                    <Col sm={6}>
         <div class="contact">
           <div class="form-container">
             <h2>Contact Me</h2>
@@ -19,13 +25,15 @@ function Connect() {
                 <label>Email*</label>
               </div>
               <div class="input-field">
-                <textarea name="message" rows="5" required=""></textarea>
+                <textarea name="message" rows="4" required=""></textarea>
                 <label>Message*</label>
               </div>
               <input type="submit" name="" value="Send Message" class="btn" />
             </form>
           </div>
         </div>
+        </Col>
+        <Col sm={6}>
         <div class="quote">
           <h2><b>
           Want to connect?<br />
@@ -38,6 +46,9 @@ function Connect() {
           <br/>
           <br/>
         </div>
+        </Col>
+        </Row>
+        </Container>
       </section>
       <Footer />
       </div>
